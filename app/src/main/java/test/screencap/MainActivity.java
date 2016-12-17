9bae1b8cbb6a6245b313086e735c422d5bc3fc8c
Mihai Corciu <free_roaming94@yahoo.com>
2016-12-17 16:53:49 +0200

    Service starting when buttons are pressed.

--- app/src/main/java/test/screencap/MainActivity.java
+++ app/src/main/java/test/screencap/MainActivity.java
@@ -3,6 +3,7 @@ package test.screencap;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +11,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Intent service = new Intent(getApplicationContext(), ScreenCapService.class);
-        this.startService(service);
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent key){
+        int action = key.getAction();
+        int keyCode = key.getKeyCode();
+
+        switch(keyCode){
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                if(action == KeyEvent.ACTION_UP){
+                    System.out.println("Pressed VOLUME UP");
+                    Intent service = new Intent(getApplicationContext(), ScreenCapService.class);
+                    this.startService(service);
+
+                }
+                break;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if(action == KeyEvent.ACTION_DOWN) {
+                    System.out.println("Pressed VOLUME DOWN");
+                    Intent service = new Intent(getApplicationContext(), ScreenCapService.class);
+                    this.startService(service);
+
+                }
+                break;
+            default:
+                return super.dispatchKeyEvent(key);
+        }
+        return super.dispatchKeyEvent(key);
     }
 
 
