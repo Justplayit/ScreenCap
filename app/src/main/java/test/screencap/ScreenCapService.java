7583f592798d9ea9179d7f794bb9f234ef06b259
Mihai Corciu <free_roaming94@yahoo.com>
2016-12-17 16:24:57 +0200

    Implemented Toast.

---
+++ app/src/main/java/test/screencap/ScreenCapService.java
@@ -0,0 +1,40 @@
+package test.screencap;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Looper;
+import android.widget.Toast;
+
+/**
+ * Created by free_ on 12/17/2016.
+ */
+
+public class ScreenCapService extends IntentService {
+
+    public ScreenCapService(){
+        this(ScreenCapService.class.getName());
+    }
+
+    public ScreenCapService(String name) {
+        super(name);
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        showToast("Startig ScreenCap service...");
+
+
+        showToast("Shutting down ScreenCap service...");
+    }
+
+    private void showToast(final String text){
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
