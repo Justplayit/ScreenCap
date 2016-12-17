3887be956eacd44cb3353bdd00d78756f0973aca
Mihai Corciu <free_roaming94@yahoo.com>
2016-12-17 19:40:18 +0200

    Successfully implemented Service Bind. Implemented snapshot method.

--- app/src/main/java/test/screencap/MainActivity.java
+++ app/src/main/java/test/screencap/MainActivity.java
@@ -1,44 +1,111 @@
 package test.screencap;
 
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.KeyEvent;
 
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+    private ScreenCapService screenCap;
+    boolean bound = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        try {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+        catch (Exception exception){
+            System.out.println("Exception occurred in \"MainActivity - onCreate()\" > " +
+                exception);
+        }
+    }
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            try {
+                ScreenCapService.LocalBinder binder = (ScreenCapService.LocalBinder) service;
+                screenCap = binder.getService();
+                bound = true;
+            }
+            catch (Exception exception){
+                System.out.println("Error occurred in \"MainActivity - onServiceConnected()\" > " +
+                    exception);
+            }
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+            bound = false;
+        }
+    };
+
+    @Override
+    protected void onStart(){
+        try {
+            super.onStart();
+
+            Intent intent = new Intent(this, ScreenCapService.class);
+            bindService(intent, connection, Context.BIND_AUTO_CREATE);
+        }
+        catch (Exception exception){
+            System.out.println("Error occurred in \"MainActivity - onStart()\" > " +
+                exception);
+        }
+    }
+
+    @Override
+    protected void onStop(){
+        try {
+            super.onStop();
+            if (bound) {
+                unbindService(connection);
+                bound = false;
+            }
+        }
+        catch(Exception exception){
+            System.out.println("Error occurred in \"MainActivity - onStop()\" > " +
+                exception);
+        }
     }
 
     @Override
     public boolean dispatchKeyEvent(KeyEvent key){
-        int action = key.getAction();
-        int keyCode = key.getKeyCode();
+        try {
+            int action = key.getAction();
+            int keyCode = key.getKeyCode();
 
-        switch(keyCode){
-            case KeyEvent.KEYCODE_VOLUME_UP:
-                if(action == KeyEvent.ACTION_UP){
-                    System.out.println("Pressed VOLUME UP");
-                    Intent service = new Intent(getApplicationContext(), ScreenCapService.class);
-                    this.startService(service);
+            switch (keyCode) {
+                case KeyEvent.KEYCODE_VOLUME_UP:
+                    if (action == KeyEvent.ACTION_UP) {
+                        System.out.println("Pressed VOLUME UP");
+                        if (bound) {
+                            try {
+                                screenCap.takeSnapshot(this);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                    break;
 
-                }
-                break;
-            case KeyEvent.KEYCODE_VOLUME_DOWN:
-                if(action == KeyEvent.ACTION_DOWN) {
-                    System.out.println("Pressed VOLUME DOWN");
-                    Intent service = new Intent(getApplicationContext(), ScreenCapService.class);
-                    this.startService(service);
-
-                }
-                break;
-            default:
-                return super.dispatchKeyEvent(key);
+                default:
+                    return false;
+            }
         }
-        return super.dispatchKeyEvent(key);
+        catch (Exception exception){
+            System.out.println("Error occurred in \"MainActivity - dispatchKeyEvent()\" > " +
+                exception);
+        }
+
+        return false;
     }
 
 
--- app/src/main/java/test/screencap/ScreenCapService.java
+++ app/src/main/java/test/screencap/ScreenCapService.java
@@ -1,41 +1,72 @@
 package test.screencap;
 
-import android.app.IntentService;
+import android.app.Activity;
+import android.app.Service;
 import android.content.Intent;
-import android.os.Handler;
-import android.os.Looper;
-import android.widget.Toast;
+import android.graphics.Bitmap;
+import android.os.Binder;
+import android.os.Environment;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.view.View;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
 
 /**
  * Created by free_ on 12/17/2016.
  */
 
-public class ScreenCapService extends IntentService {
+public class ScreenCapService extends Service {
+    private final IBinder binder =  new LocalBinder();
 
-    public ScreenCapService(){
-        this(ScreenCapService.class.getName());
-    }
-
-    public ScreenCapService(String name) {
-        super(name);
-    }
-
+    @Nullable
     @Override
-    protected void onHandleIntent(Intent intent) {
-        showToast("Startig ScreenCap service...");
-
-
-
-        showToast("Shutting down ScreenCap service...");
+    public IBinder onBind(Intent intent) {
+        return binder;
     }
 
-    private void showToast(final String text){
-        Handler handler = new Handler(Looper.getMainLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT).show();
+    public class LocalBinder extends Binder{
+        ScreenCapService getService(){
+            return ScreenCapService.this;
+        }
+    }
+
+    protected void takeSnapshot(Activity activity) throws IOException {
+        try {
+            Date date = new Date();
+            android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", date);
+
+            String sdPath = Environment.getExternalStorageDirectory().toString();
+
+            File sdDir = new File(sdPath + "/ScreenCap/");
+            if (!sdDir.exists()) {
+                sdDir.mkdir();
             }
-        });
+
+            View view = activity.getWindow().getDecorView().getRootView();
+            view.setDrawingCacheEnabled(true);
+
+            Bitmap bitmap = Bitmap.createBitmap(view.getDrawingCache());
+            view.setDrawingCacheEnabled(false);
+
+            File image = new File(sdPath + "/ScreenCap/" + date + ".jpg");
+
+            FileOutputStream outputStream = new FileOutputStream(image);
+
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+
+            outputStream.flush();
+            outputStream.close();
+
+            System.out.println("I took a snapshot... It felt great...");
+        }
+        catch (Exception exception){
+            System.out.println("Exception occured in \"takeSnapshot\" > " +
+                    exception);
+        }
     }
 }
+
+
